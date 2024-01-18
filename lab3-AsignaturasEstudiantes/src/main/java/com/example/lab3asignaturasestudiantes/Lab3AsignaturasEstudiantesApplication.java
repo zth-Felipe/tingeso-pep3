c97071457377ae7555d31229b7c64d6df9cef543
zth-Felipe <92702735+zth-Felipe@users.noreply.github.com>
2024-01-17 23:22:26 -0300

    Add files via upload

---
+++ lab3-AsignaturasEstudiantes/src/main/java/com/example/lab3asignaturasestudiantes/Lab3AsignaturasEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab3asignaturasestudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3AsignaturasEstudiantesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3AsignaturasEstudiantesApplication.class, args);
+    }
+
+}
